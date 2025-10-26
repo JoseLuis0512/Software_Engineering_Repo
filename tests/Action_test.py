961d0c8e16671c409707c9f994c799277ac74c84
Lu0512 <joseluisito0512@gmail.com>
2025-10-25 21:32:20 -0600

    Changes to prove

--- tests/Action_test.py
+++ tests/Action_test.py
@@ -2,7 +2,7 @@ import os
 from Flow.Action import Result, save_action, print_action
 
 def test_result_dataclass():
-    r = Result(100, 0.1, 10, 110)
+    r = Result(101, 0.1, 10, 110)
     assert r.amount == 101
     assert r.tax == 0.1
     assert r.taxes == 10
